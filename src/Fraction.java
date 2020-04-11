5013aee8c45769ca1663099665496b4aa2f6441c
Murshid  Omer <50660562+murshidomer@users.noreply.github.com>
2020-04-11 20:35:22 +0530

    classes created

--- src/Fraction.java
+++ src/Fraction.java
@@ -5,22 +5,22 @@ import static java.lang.StrictMath.copySign;
 
 public class Fraction {
     //hold numberator, denominator
-    private int numerator ;
-    private int denominator ;
+    private double numerator ;
+    private double denominator ;
 
 
-    public Fraction(int numerator, int denominator) throws IllegalArgumentException {
+    public Fraction(double numerator, double denominator) throws IllegalArgumentException {
         this.numerator = numerator;
         this.denominator = denominator;
         if (denominator == 0) throw new IllegalArgumentException();
-        if (denominator < 0) StrictMath.copySign(numerator, denominator);
+        if (denominator < 0) copySign(numerator, denominator);
     }
 
 
     public static void main(String[] args) {
         System.out.println("Enter n % d ");
         Scanner scanner = new Scanner(System.in);
-        int numerator, denominator;
+        double numerator, denominator;
         numerator = scanner.nextInt();
         denominator = scanner.nextInt();
         Fraction fraction = new Fraction(numerator, denominator);
