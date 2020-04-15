86f973742f74e7e6b913d9442b3e7da3c345cf7b
Murshid  Omer <50660562+murshidomer@users.noreply.github.com>
2020-04-15 17:59:34 +0530

    Flipsign (#1)
    * classes created
    
    * added methods:
    toDouble
    toString
    toLowestTerms()
    gcd()
    add()

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,30 +1,101 @@
+import java.util.Objects;
 import java.util.Scanner;
-
-import static java.lang.StrictMath.abs;
-import static java.lang.StrictMath.copySign;
 
 public class Fraction {
     //hold numberator, denominator
-    private int numerator ;
-    private int denominator ;
+    private int numerator;
+    private int denominator;
 
+
+    public Fraction() {
+        numerator = 0;
+        denominator = 1;
+    }
+
+    public Fraction(int numerator) {
+        this.numerator = numerator;
+        denominator = 1;
+    }
 
     public Fraction(int numerator, int denominator) throws IllegalArgumentException {
         this.numerator = numerator;
         this.denominator = denominator;
         if (denominator == 0) throw new IllegalArgumentException();
-        if (denominator < 0) StrictMath.copySign(numerator, denominator);
+        if (denominator < 0) {
+            this.numerator *= Integer.signum(denominator);
+            this.denominator *= Integer.signum(denominator);
+        }
+
     }
 
+    public void toLowestTerms() {
+        int divisor = gcd(this.numerator, this.denominator);
+        this.numerator /= divisor;
+        this.denominator /= divisor;
+
+    }
+
+    public static int gcd(int num, int den) {
+        int remainder = 0, temp = 0;
+        while (!(num == 0 || den == 0)) {
+            temp = num;
+            remainder = num % den;
+            num = den;
+            den = remainder;
+        }
+        return num;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Enter n % d ");
+        System.out.println("Enter fraction ");
         Scanner scanner = new Scanner(System.in);
         int numerator, denominator;
         numerator = scanner.nextInt();
         denominator = scanner.nextInt();
         Fraction fraction = new Fraction(numerator, denominator);
     }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public double toDouble() {
+        double result = numerator / denominator;
+        return result;
+    }
+
+    public Fraction add(Fraction other) {
+        Fraction additionResult = new Fraction();
+        additionResult.numerator =
+                (this.numerator * other.denominator) + (other.numerator * this.denominator);
+        additionResult.denominator = this.denominator * other.denominator;
+        additionResult.toLowestTerms();
+        return additionResult;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return numerator == fraction.numerator &&
+                denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
+    }
+
 
 }
 
--- src/FractionCalculator.java
+++ src/FractionCalculator.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
+
 public class FractionCalculator {
     //enter fractoins and operations, calculate result
     //run until told to quit
+
+    public String getOperation(Scanner scanner) {
+        String s = null;
+        return s;
+    }
+
+/*    public Fraction getFraction(Scanner scanner) {
+
+    }
+    public boolean validtFraction(String input) {
+
+    }*/
+
+
+    public static void main(String[] args) {
+        System.out.println("This program is a fraction calculator \n" +
+                "It will add subtract, multply and divide fractions until you type Q to quit \n" +
+                "Please enter your fractions in the form a/b, where a and b are integers. \n" +
+                "-------------------------------------------------------------------------------\n" +
+                "Please enter an operation (+, -, /, *, = or Q to quit):");
+        int a = 1/2;
+        int b = 1/2;
+        int c = a + b;
+        System.out.println(c);
+
+    }
 }
